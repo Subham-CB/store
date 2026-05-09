99c067e57c00689e5d22946815403ca23f83b4ee
Subham <de.subham1@gmail.com>
2026-05-10 00:07:59 +0200

    added tests for customer name search endpoint

--- src/test/java/com/example/store/controller/CustomerControllerTests.java
+++ src/test/java/com/example/store/controller/CustomerControllerTests.java
@@ -1,22 +1,28 @@
 package com.example.store.controller;
 
+import com.example.store.component.CustomerSearchProps;
 import com.example.store.dto.CustomerDTO;
 import com.example.store.dto.CustomerRequestDTO;
 import com.example.store.mapper.CustomerMapper;
 import com.example.store.service.CustomerService;
+import com.example.store.util.PageableBuilder;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -34,8 +40,14 @@ class CustomerControllerTests {
     @MockitoBean
     private CustomerService customerService;
 
+    @MockitoBean
+    private CustomerSearchProps customerSearchProps;
+
+    @MockitoBean
+    private PageableBuilder pageableBuilder;
+
     private CustomerDTO customerDTO;
-    private CustomerRequestDTO customerRequestDTO;
+    private Pageable mockPageable;
 
     @BeforeEach
     void setUp() {
@@ -43,26 +55,53 @@ class CustomerControllerTests {
         customerDTO.setName("John Doe");
         customerDTO.setId(1L);
 
-        customerRequestDTO = new CustomerRequestDTO();
-        customerRequestDTO.setName("John Doe");
+        mockPageable = PageRequest.of(0,10);
+
+        when(customerSearchProps.getLimit()).thenReturn(20);
+        when(customerSearchProps.getSortField()).thenReturn("id");
+        when(customerSearchProps.getDirection()).thenReturn("asc");
+
+        when(pageableBuilder.buildPageable(any(), any(), any(), any(), anyInt(), anyString(), anyString()))
+                .thenReturn(mockPageable);
+
     }
 
     @Test
+    @DisplayName("POST /customer - Should return 201 Created")
     void testCreateCustomer() throws Exception {
+
+        CustomerRequestDTO customerRequestDTO = new CustomerRequestDTO();
+        customerRequestDTO.setName("John Doe");
+
         when(customerService.createCustomer(customerRequestDTO)).thenReturn(customerDTO);
 
         mockMvc.perform(post("/customer")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(customerRequestDTO)))
                 .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(1))
                 .andExpect(jsonPath("$.name").value("John Doe"));
     }
 
     @Test
-    void testGetAllCustomers() throws Exception {
-        when(customerService.findAllCustomers()).thenReturn(List.of(customerDTO));
+    @DisplayName("GET /customer - Find all (no name param)")
+    void testGetCustomers_All() throws Exception {
+        when(customerService.findCustomers(mockPageable)).thenReturn(List.of(customerDTO));
 
         mockMvc.perform(get("/customer"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value(1))
+                .andExpect(jsonPath("$[0].name").value("John Doe"));
+    }
+
+    @Test
+    @DisplayName("GET /customer - Search by name")
+    void testGetCustomers_WithName() throws Exception{
+        String searchName = "John";
+        when(customerService.findCustomersNameContainingSubString(mockPageable,searchName)).thenReturn(List.of(customerDTO));
+
+        mockMvc.perform(get("/customer")
+                        .param("name",searchName))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[0].id").value(1))
                 .andExpect(jsonPath("$[0].name").value("John Doe"));
