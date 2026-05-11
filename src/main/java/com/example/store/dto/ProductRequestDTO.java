7f8b07c85612735933dbe80920850db28563c872
Subham <de.subham1@gmail.com>
2026-05-11 03:47:28 +0200

    add ProductDTO, ProductRequestDTO, and ProductMapper

---
+++ src/main/java/com/example/store/dto/ProductRequestDTO.java
@@ -0,0 +1,17 @@
+package com.example.store.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductRequestDTO {
+
+    @NotBlank(message = "Description is required")
+    @Length(max = 225, message = "Description is too long")
+    private String description;
+}
---
+++ src/main/java/com/example/store/mapper/ProductMapper.java
@@ -0,0 +1,30 @@
+package com.example.store.mapper;
+
+import com.example.store.dto.ProductDTO;
+import com.example.store.dto.ProductRequestDTO;
+import com.example.store.entity.Order;
+import com.example.store.entity.Product;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+
+    @Mapping(target = "orderIds", source = "orders")
+    ProductDTO productToProductDTO(Product product);
+
+    List<ProductDTO> productsToProductDTOs(List<Product> products);
+
+    @Mapping(target = "id",ignore = true)
+    @Mapping(target = "orders",ignore = true)
+    Product productRequestDTOToProduct(ProductRequestDTO productRequestDTO);
+
+
+    default List<Long> mapOrdersToIds(List<Order> orders){
+        return orders.stream()
+                .map(Order::getId)
+                .toList();
+    }
+}
